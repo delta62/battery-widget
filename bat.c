28fd01677dc146c3713f50a65529ee2b2da6cbf7
Sam Noedel <sam.noedel@gmail.com>
2017-07-10 19:23:11 -0700

    Dynamic charging indicator

--- bat.c
+++ bat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "bat.h"
 
@@ -10,9 +11,9 @@ int main(int argc, char **argv)
 	get_opt_info(argc, argv, opts);
 
 	struct bat_info *info = mk_bat_info();
-	get_bat_info(info);
+	get_bat_info(info, opts);
 
-	print(info, opts->fmt);
+	print(info);
 
 	destroy_opts(opts);
 	destroy_bat_info(info);
@@ -21,16 +22,22 @@ int main(int argc, char **argv)
 struct opts* mk_opts()
 {
 	struct opts *ret = (struct opts *)malloc(sizeof(struct opts));
-	ret->color = 0;
 	ret->path = "/sys/class/power_supply/BAT1";
-	ret->fmt = "%b %p%%";
 	return ret;
 }
 
 char *bat_icon(const struct bat_info *bat, const unsigned short len)
 {
 	char *buf = (char *)malloc(len + 4);
-	buf[0] = '+';
+	switch (bat->status) {
+		case CHARGING:
+		case CHARGED:
+			buf[0] = '+';
+			break;
+		default:
+			buf[0] = '-';
+			break;
+	}
 	buf[1] = '[';
 	buf[len + 2] = ']';
 	buf[len + 3] = '\0';
@@ -40,17 +47,17 @@ char *bat_icon(const struct bat_info *bat, const unsigned short len)
 
 	for (unsigned short i = 0; i < len; i++) {
 		char c = ' ';
-		if (pct >= 100 || step * (len - i) < pct) c = '|';
+		if (step * (len - i) <= pct) c = '|';
 		buf[i + 2] = c;
 	}
 
 	return buf;
 }
 
-void print(const struct bat_info *bat, const char *fmt)
+void print(const struct bat_info *bat)
 {
 	int pct = (int)((100.0 * bat->cur) / bat->max);
-	char * icon = bat_icon(bat, 4);
+	char * icon = bat_icon(bat, 8);
 	printf("%s (%d%%)\n", icon, pct);
 	free(icon);
 }
@@ -58,14 +65,8 @@ void print(const struct bat_info *bat, const char *fmt)
 int get_opt_info(const int argc, char **argv, struct opts *opts)
 {
 	int opt;
-	while ((opt = getopt(argc, argv, "cf:p:")) != -1) {
+	while ((opt = getopt(argc, argv, "p:")) != -1) {
 		switch (opt) {
-			case 'c':
-				opts->color = 1;
-				break;
-			case 'f':
-				opts->fmt = optarg;
-				break;
 			case 'p':
 				opts->path = optarg;
 				break;
@@ -86,6 +87,7 @@ struct bat_info *mk_bat_info()
 	struct bat_info *ret = (struct bat_info *)malloc(sizeof(struct bat_info));
 	ret->max = 0;
 	ret->cur = 0;
+	ret->status = CHARGED;
 	return ret;
 }
 
@@ -94,11 +96,31 @@ void destroy_bat_info(struct bat_info *info)
 	if (info != NULL) free(info);
 }
 
-void get_bat_info(struct bat_info *info)
+void get_bat_info(struct bat_info *info, struct opts *opts)
 {
-	FILE *maxFile = fopen("/sys/class/power_supply/BAT1/energy_full", "r");
-	FILE *curFile = fopen("/sys/class/power_supply/BAT1/energy_now", "r");
+	int pathlen = strlen(opts->path) + 13;
+	char path[pathlen];
+	snprintf(path, pathlen, "%s/%s", opts->path, "energy_full");
+	FILE *maxFile = fopen(path, "r");
+	snprintf(path, pathlen, "%s/%s", opts->path, "energy_now");
+	FILE *curFile = fopen(path, "r");
+
+	char status[56];
+	snprintf(path, pathlen, "%s/%s", opts->path, "status");
+	FILE *statusFile = fopen(path, "r");
+	fread(status, 1, 55, statusFile);
+	if (strcmp(status, "Charging\n") == 0) {
+		info->status = CHARGING;
+	} else if (strcmp(status, "Discharging\n") == 0) {
+		info->status = DISCHARGING;
+	} else {
+		info->status = CHARGED;
+	}
 
 	fscanf(maxFile, "%u", &info->max);
 	fscanf(curFile, "%u", &info->cur);
+
+	fclose(maxFile);
+	fclose(curFile);
+	fclose(statusFile);
 }
--- bat.h
+++ bat.h
@@ -1,26 +1,31 @@
 #ifndef __BAT_H__
 #define __BAT_H__
 
+enum bat_status {
+  CHARGING,
+  CHARGED,
+  DISCHARGING
+};
+
 struct bat_info {
   unsigned int max;
   unsigned int cur;
+  enum bat_status status;
+};
+
+struct opts {
+  char *path;
 };
 
 struct bat_info *mk_bat_info();
 void destroy_bat_info(struct bat_info *info);
-void get_bat_info(struct bat_info *info);
-
-struct opts {
-  unsigned short color;
-  char *path;
-  char *fmt;
-};
+void get_bat_info(struct bat_info *info, struct opts *opts);
 
 struct opts *mk_opts();
 int get_opt_info(const int argc, char **argv, struct opts *opts);
 void destroy_opts(struct opts *opts);
 
-void print(const struct bat_info *bat, const char *fmt);
+void print(const struct bat_info *bat);
 char *bat_icon(const struct bat_info *bat, const unsigned short len);
 
 #endif
